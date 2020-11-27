6a45853dce9d1324d260f4273a72600bce881118
Btojaka <bruthtoj@gmail.com>
2020-11-27 14:44:54 +0100

    Se continua con la validacion y modificaciones en html

--- tesoro.js
+++ tesoro.js
@@ -5,17 +5,64 @@ document.addEventListener(
 
         //validacion del nombre introducido
         let validarNombre = () =>{
-            let elemento = document.getElementById("nombre");
+            let elemento = document.getElementById("name");
+            borrarError('errorName', elemento);
             if(!elemento.checkValidity()) {
                 if(elemento.validity.valueMissing){
-                    error(elemento, "Debe introducir un nombre", 'alerta');
+                    error(elemento, "Debes introducir un nombre", 'errorName');
+                    
+                    // hay que distinguir entre cuando faltan letras y cuanodo aparecen numeros
                 }else if(elemento.validity.patternMismatch){
-                    if(elemento < )
-
+                    //alert("EL nombre debe tener más de 4 letras");
+                    alert("Números no permitidos");
                 }
+                return false;
             }
+            return true;
         }
+
+        let validacionFinal = () => {
+            if(validarNombre) {
+              return true; 
+          } else {
+            //console.log("prevent default")
+            e.preventDefault();
+            return false;
+          }
+        }
+
+            //error debajo del input
+        let error = (elemento, mensaje, parrafo) => {
+            document.getElementById(parrafo).innerHTML = mensaje;
+            document.getElementById(parrafo).className = 'wrong'
+            elemento.style.border = 'solid 2px rgb(214, 86, 118)';
+            //elemento.focus(); no hace falta
+        }
+         // alert
+        // let error2 = (elemento, mensaje, parrafo) => {
+        //     document.getElementById(parrafo).innerHTML = mensaje;
+        //     document.getElementById(parrafo).className = 'wrong'
+        //     elemento.style.border = 'solid 2px rgb(214, 86, 118)';
+        //     //elemento.focus(); no hace falta
+        // }
+    
+        let borrarError = (parrafo, elemento) => {
+            document.getElementById(parrafo).innerHTML = "";
+            elemento.style.border = '1px solid #ccc';
         
+        }
+
+        //realizar validaciones
+        let validar = (e) => {
+            validarNombre();
+            validacionFinal()
+            }
+
+
+        //el botón enviar inicia la función validar
+        document.getElementById("enviar").addEventListener('click', validar, false);
+
+
     },
     false
 );
